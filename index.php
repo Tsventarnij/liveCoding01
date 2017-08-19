6033e8e112d8c663f3181ff1490d8474921be963
tsventarnij <tsventarnij@gmail.com>
2017-08-19 16:57:28 +0300

    LiveCoding

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$dir=(isset($_GET["dir"])?$_GET["dir"]:$dir="");
+$dirRoot=$_SERVER['DOCUMENT_ROOT'];
+if($dir!=""){
+  if($dir[0]=="/") $dir=substr($dir,1);
+  $chars = preg_split('~/~', $dir);
+  if($chars[count($chars)-1]=='.') {
+    unset($chars[count($chars)-1]);
+    unset($chars[count($chars)-1]);
+  }
+  if(count($chars)>0){
+    if($chars[count($chars)-1]=='..') {
+      unset($chars[count($chars)-1]);
+      unset($chars[count($chars)-1]);
+      unset($chars[count($chars)-1]);
+    }
+  }
+  $dir=implode('/',$chars);
+}
+$files = scandir($dirRoot."/".$dir);
+echo "<p>Текущий каталог: ".$dirRoot."/".$dir."</p>";
+echo "<ul>";
+foreach ($files as $value)
+ {
+    preg_match("/[\d]+/", $value,$match);
+    if($match==NULL)
+    {
+      echo "<li>";
+      if (is_dir($dirRoot."/".$dir."/".$value)){
+        echo "<a href=\"\\?dir=".$dir."/".$value."\">".$value."</a>";
+      }else{
+        echo $value."(".filesize($dirRoot."/".$dir."/".$value)." байт)";
+      }
+    }
+  echo "</li>";
+}
+echo "</ul>";
+ ?>
