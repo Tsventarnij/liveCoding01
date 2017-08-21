2611a47b50d45d11698a207d792396f44279b9a1
tsventarnij <tsventarnij@gmail.com>
2017-08-21 20:58:41 +0300

    Реализованно добавления папок

--- index.php
+++ index.php
@@ -1,19 +1,59 @@
 ﻿<?php
+
 $dir=(isset($_GET["dir"])?$_GET["dir"]:$dir="");
 $dirRoot=substr($_SERVER['SCRIPT_FILENAME'], 0, strripos($_SERVER['SCRIPT_FILENAME'], "/"));
+
+// создания папок
+if(count(scandir($dirRoot))<12){
+   $sNameFold = file_get_contents("nameFolder.txt");
+   $aNameFold = preg_split('~,~', $sNameFold);
+   for($i=0; $i<count($aNameFold); $i++){
+     $aNameFold[$i]=trim($aNameFold[$i]);
+   }
+   $tempFold=[];
+   for($i=0; $i<13; $i++)
+   {
+     $rand=rand(1, count($aNameFold))-1;	
+     if($aNameFold[$rand]!=="")  mkdir($aNameFold[$rand]);
+     else $i--;
+     $tempFold[]=$aNameFold[$rand];
+     $aNameFold[$rand]="";
+   }
+   $i="";
+   $t=0;
+   foreach($aNameFold as $nameFold){
+    
+      if($nameFold!=""){
+	
+      	if($i==""){
+	   $rand=rand(0,count($tempFold)-1);
+	   mkdir($tempFold[$rand]."/".$nameFold);
+
+	   if($t==0){
+		$t=1;
+	   	$i=$nameFold;
+	   } else $t=0;
+        }
+      	else{
+	   mkdir($tempFold[$rand]."/".$i."/".$nameFold);
+
+	   $i="";
+	}
+      }
+   }
+}
+// файловый менеджер
+
 
 if($dir!=""){
   if($dir[0]=="/") $dir=substr($dir,1);
   $chars = preg_split('~/~', $dir);
   if($chars[count($chars)-1]=='.') {
-    unset($chars[count($chars)-1]);
-    unset($chars[count($chars)-1]);
+    unset($chars[count($chars)-2], $chars[count($chars)-1]);
   }
   if(count($chars)>0){
     if($chars[count($chars)-1]=='..') {
-      unset($chars[count($chars)-1]);
-      unset($chars[count($chars)-1]);
-      unset($chars[count($chars)-1]);
+      unset($chars[count($chars)-3], $chars[count($chars)-2], $chars[count($chars)-1]);
     }
   }
   $dir=implode('/',$chars);
