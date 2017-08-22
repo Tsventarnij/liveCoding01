dd937590fbbde60a1337e05e54c7dcb4874d978e
tsventarnij <tsventarnij@gmail.com>
2017-08-22 19:18:36 +0300

    Почистил код

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 ﻿<?php
-
 $dir=(isset($_GET["dir"])?$_GET["dir"]:$dir="");
 $dirRoot=substr($_SERVER['SCRIPT_FILENAME'], 0, strripos($_SERVER['SCRIPT_FILENAME'], "/"));
-
 // создания папок
 if(count(scandir($dirRoot))<12){
    $sNameFold = file_get_contents("nameFolder.txt");
@@ -11,40 +9,32 @@ if(count(scandir($dirRoot))<12){
      $aNameFold[$i]=trim($aNameFold[$i]);
    }
    $tempFold=[];
-   for($i=0; $i<13; $i++)
-   {
+   for($i=0; $i<13; $i++){
      $rand=rand(1, count($aNameFold))-1;
      if($aNameFold[$rand]!=="")  mkdir($aNameFold[$rand]);
      else $i--;
      $tempFold[]=$aNameFold[$rand];
      $aNameFold[$rand]="";
    }
-   $i="";
-   $t=0;
+   $i=""; $t=0;
    foreach($aNameFold as $nameFold){
-
       if($nameFold!=""){
-
       	if($i==""){
-	   $rand=rand(0,count($tempFold)-1);
-	   mkdir($tempFold[$rand]."/".$nameFold);
-
-	   if($t==0){
-		$t=1;
-	   	$i=$nameFold;
-	   } else $t=0;
+	         $rand=rand(0,count($tempFold)-1);
+	         mkdir($tempFold[$rand]."/".$nameFold);
+	         if($t==0){
+		           $t=1;
+	   	         $i=$nameFold;
+	         }else $t=0;
         }
       	else{
-	   mkdir($tempFold[$rand]."/".$i."/".$nameFold);
-
-	   $i="";
-	}
+	         mkdir($tempFold[$rand]."/".$i."/".$nameFold);
+	         $i="";
+	      }
       }
    }
 }
 // файловый менеджер
-
-
 if($dir!=""){
   if($dir[0]=="/") $dir=substr($dir,1);
   $chars = preg_split('~/~', $dir);
@@ -59,7 +49,7 @@ if($dir!=""){
   }
   $dir=implode('/',$chars);
 }
-$files = scandir($dirRoot."/".$dir, SCANDIR_SORT_NONE);
+$files = scandir($dirRoot."/".$dir);
 echo "<p>Текущий каталог: ".$dirRoot."/".$dir."</p>";
 echo "<ul>";
 foreach ($files as $value)
@@ -76,8 +66,7 @@ foreach ($files as $value)
     }else{
       echo $value;
     }
-
   echo "</li>";
 }
 echo "</ul>";
- ?>
+?>
